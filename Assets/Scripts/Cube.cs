e53692e3a991ff36863572640d2bf4b4f70e4567
Nikolay Voronin <wnn@yandex.ru>
2025-02-04 16:53:15 +0300

    color new cube

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,16 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+    public Color ColorNewCube { get; private set; }
+
     private void Start()
     {
-        
+        ColorNewCube = RandomColor();
     }
 
-    private void Update()
+    private Color RandomColor()
     {
-        
+        Color randomColor = new Color(Random.Range(0f, 1f),Random.Range(0f, 1f),Random.Range(0f, 1f),1);
+
+        return randomColor;
     }
 }
