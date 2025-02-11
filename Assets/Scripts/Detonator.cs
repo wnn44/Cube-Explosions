ddd86c21349017178f560a3ad32ec4bf17bff852
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2025-02-11 22:47:29 +0300

    дополнил силу вертикального подбрасывания

--- Assets/Scripts/Detonator.cs
+++ Assets/Scripts/Detonator.cs
@@ -5,12 +5,13 @@ public class Detonator : MonoBehaviour
 {
     [SerializeField] private float _explosionRadius;
     [SerializeField] private float _explosionForce;
+    [SerializeField] private float _upwardModifier;
 
     public void Explode(List<Rigidbody> allCubes)
     {
         foreach (Rigidbody explodubleObject in allCubes)
         {
-            explodubleObject.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+            explodubleObject.AddExplosionForce(_explosionForce, transform.position, _explosionRadius, _upwardModifier);
         }
     }
 }
