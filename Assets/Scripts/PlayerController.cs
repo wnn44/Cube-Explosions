58a9b6952fc32aca1e8aa3cc3437c92d0a9a4035
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2025-02-14 19:39:23 +0300

    Зачет

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     private readonly int _leftKeyMouse = 0;
     private int _maxChance = 100;
+    private int _minChance = 1;
     private Camera _camera;
 
     private void Awake()
@@ -17,19 +17,15 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        List<Rigidbody> newCubes;
-
         if (Input.GetMouseButtonDown(_leftKeyMouse))
         {
             Cube cube = SelectCubeInScene();
 
             if (cube != null)
             {
-                if (Random.Range(1, _maxChance) <= cube.ChanceSeparation)
+                if (Random.Range(_minChance, _maxChance) <= cube.ChanceSeparation)
                 {
-                    newCubes = _spawner.Spawn(cube);
-
-                    _blasting.Explode(newCubes);
+                     _blasting.Explode(_spawner.Spawn(cube));
                 }
 
                 Destroy(cube.gameObject);
@@ -41,10 +37,9 @@ public class PlayerController : MonoBehaviour
     {
         Cube cube = null;
 
-        RaycastHit hit;
         Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
             Transform objectHit = hit.transform;
 
